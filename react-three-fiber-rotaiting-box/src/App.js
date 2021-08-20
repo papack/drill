8640053a10e5821fb5c44834322a23b580599fda
Matthias Steiner <m@kjpjm.de>
2021-08-20 05:37:18 +0200

    recreate box

--- react-three-fiber-rotaiting-box/src/App.js
+++ react-three-fiber-rotaiting-box/src/App.js
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import style from "./styles/App.module.css";
+import { useFrame, Canvas } from "@react-three/fiber";
+import { a, useSpring } from "@react-spring/three";
+
+const Box = () => {
+  const ref = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  const { color, scale } = useSpring({
+    scale: active ? 1.5 : 1,
+    color: hovered ? "hotpink" : "orange",
+  });
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 0.01;
+  });
+
+  return (
+    <a.mesh
+      ref={ref}
+      scale={scale}
+      onClick={() => {
+        setActive(!active);
+      }}
+      onPointerOver={() => {
+        setHover(true);
+      }}
+      onPointerOut={() => {
+        setHover(false);
+      }}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <a.meshStandardMaterial color={color} />
+    </a.mesh>
+  );
+};
 
 export default function App() {
-  return <div className={style.container}>no content yet</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.canvasContainer}>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
+      </div>
+    </div>
+  );
 }
