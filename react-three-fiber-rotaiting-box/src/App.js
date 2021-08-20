5ead4231cf1f7bea0a6859b7b5f87b3683a02db7
Matthias Steiner <m@kjpjm.de>
2021-08-20 05:09:51 +0200

    remove cube

--- react-three-fiber-rotaiting-box/src/App.js
+++ react-three-fiber-rotaiting-box/src/App.js
@@ -1,54 +1,6 @@
+import React from "react";
 import style from "./styles/App.module.css";
-import { useState, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useSpring, a } from "@react-spring/three";
 
-const Box = () => {
-  const [hovered, setHovered] = useState(false);
-  const [active, setActive] = useState(false);
-  const ref = useRef();
-  const animation = useSpring({
-    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-    color: hovered ? "hotpink" : "gray",
-  });
-
-  useFrame((state, delta) => {
-    ref.current.rotation.y += 0.01;
-    ref.current.rotation.z += 0.01;
-  });
-
-  return (
-    <a.mesh
-      ref={ref}
-      onPointerOver={() => {
-        setHovered(true);
-      }}
-      onPointerOut={() => {
-        setHovered(false);
-      }}
-      onClick={() => {
-        setActive(!active);
-      }}
-      scale={animation.scale}
-    >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <a.meshStandardMaterial attach="material" color={animation.color} />
-    </a.mesh>
-  );
-};
-
-function App() {
-  return (
-    <div className={style.container}>
-      <div className={style.canvasContainer}>
-        <Canvas className={style.canvas}>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Box />
-        </Canvas>
-      </div>
-    </div>
-  );
+export default function App() {
+  return <div className={style.container}>no content yet</div>;
 }
-
-export default App;
