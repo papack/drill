ec8798d0363bf9b68c883d9f442186a810b0ad90
Matthias Steiner <m@kjpjm.de>
2021-08-20 15:24:42 +0200

    create rect

--- svg/src/App.js
+++ svg/src/App.js
@@ -1,7 +1,33 @@
+import { a, useSpring } from "@react-spring/web";
+import { useState } from "react";
+
 import style from "./styles/App.module.css";
 
 function App() {
-  return <div className={style.container}>no content yet</div>;
+  const [reverse, setReverse] = useState(false);
+
+  const { fill } = useSpring({
+    from: {
+      fill: "green",
+    },
+    to: { fill: "red" },
+    delay: 500,
+    reverse,
+    reset: true,
+    onRest: () => {
+      setReverse(!reverse);
+    },
+  });
+
+  return (
+    <a.div className={style.container}>
+      <div style={{ backgroundColor: "black" }}>
+        <svg width="500" height="500">
+          <a.rect style={{ fill }} x="225" y="225" width="50" height="50" />
+        </svg>
+      </div>
+    </a.div>
+  );
 }
 
 export default App;
