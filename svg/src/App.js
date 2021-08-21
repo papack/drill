607de1d64e34e6071d6669a04157b1cbf4ebc24f
Matthias Steiner <m@kjpjm.de>
2021-08-21 04:20:14 +0200

    add hover effect

--- svg/src/App.js
+++ svg/src/App.js
@@ -5,6 +5,7 @@ import style from "./styles/App.module.css";
 
 function App() {
   const [reverse, setReverse] = useState(false);
+  const [hover, setHover] = useState(false);
 
   const { fill } = useSpring({
     from: {
@@ -19,11 +20,37 @@ function App() {
     },
   });
 
+  const { width, height, transform } = useSpring({
+    width: hover ? "100" : "50",
+    height: hover ? "100" : "50",
+    transform: hover ? "translate(-50px,-50px)" : "translate(-25px,-25px)",
+  });
+
   return (
     <a.div className={style.container}>
       <div style={{ backgroundColor: "black" }}>
-        <svg width="500" height="500">
-          <a.rect style={{ fill }} x="225" y="225" width="50" height="50" />
+        <svg
+          width="500"
+          height="500"
+          viewBox="0 0 500 500"
+          preserveAspectRatio="none"
+        >
+          <a.rect
+            onPointerOver={() => {
+              setHover(true);
+            }}
+            onPointerOut={() => {
+              setHover(false);
+            }}
+            style={{
+              fill,
+              width,
+              height,
+              transform,
+            }}
+            x="250"
+            y="250"
+          />
         </svg>
       </div>
     </a.div>
